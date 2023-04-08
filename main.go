f2d0923ca3a89a2bd2624459624a37cca5c1b563
Yorvoration <dev.dilshodjon@gmail.com>
2023-04-08 17:10:05 +0500

    OmborXona

---
+++ main.go
@@ -0,0 +1,250 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	host     = "containers-us-west-87.railway.app"
+	port     = 7572
+	user	 = "postgres"
+	password = "4aMQQl5p9qnUD52lJaaL"
+	dbname   = "railway"
+)
+
+type User struct {
+	ID 	  	  int    `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Name      string `json:"name"`
+	Surname   string `json:"surname"`
+	Age       int    `json:"age"`
+	Phone     string `json:"phone"`
+	Promocode string `json:"promocode"`
+	Status    string `json:"status"`
+	Roles     string `json:"roles"`
+	City 	  string `json:"city"`
+	CreatedAt time.Time `json:"created_at"`
+	Token     string `json:"token"`
+	Blocked   bool `json:"blocked"`
+}
+
+func passwordHash(password string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return string(hash)
+}
+
+func checkPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func generateToken(username string, password string) (string, error) {
+	//jwt token generate using username and password 
+	
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": username,
+		"password": password,
+		"created_at": time.Now(),
+		"expired_at": time.Now().Add(time.Hour * 24).Unix(),
+	})
+	tokenString, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	fmt.Println(tokenString)
+	return tokenString, nil
+}
+
+
+func main() {
+	r := gin.Default()
+	r.POST("/register", register)
+	r.POST("/login", login)
+	r.POST("/logout", logout)
+
+	r.Run()
+}
+
+func connectDB() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+ "password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func register(c *gin.Context) {
+	var user User
+	err := c.BindJSON(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	db := connectDB()
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username VARCHAR(255), email VARCHAR(255), password VARCHAR(255), name VARCHAR(255), surname VARCHAR(255), age INT,  phone VARCHAR(255), promocode VARCHAR(255), status VARCHAR(255), roles VARCHAR(255), city VARCHAR(255), created_at TIMESTAMP, token VARCHAR(255), blocked BOOLEAN)")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user.Promocode = ""
+	user.Status = "active"
+	user.Roles = "user"
+	user.CreatedAt = time.Now()
+	user.Token = ""
+	user.Blocked = false
+
+	if user.Username == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "username is empty"})
+		return
+	}
+	if user.Email == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "email is empty"})
+		return
+	}
+	if user.Password == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "password is empty"})
+		return
+	}
+	if user.Name == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "name is empty"})
+		return
+	}
+	if user.Surname == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "surname is empty"})
+		return
+	}
+	if user.Phone == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "phone is empty"})
+		return
+	}
+
+	if user.Age == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "age is empty"})
+		return
+	}
+
+	if  user.Age < 16 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "age is less than 16"})
+		return
+	}
+
+	if user.City == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "city is empty"})
+		return
+	}
+
+	var username string
+	err = db.QueryRow("SELECT username FROM users WHERE username = $1", user.Username).Scan(&username)
+	if err == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "username already exist"})
+		return
+	}
+
+	_, err = db.Exec("INSERT INTO users (id, username, email, password, name, surname, age, phone, promocode, status, roles, city, created_at, token, blocked) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)", user.ID, user.Username, user.Email, passwordHash(user.Password), user.Name, user.Surname, user.Age, user.Phone, user.Promocode, user.Status, user.Roles, user.City, user.CreatedAt, user.Token, user.Blocked)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
+}
+
+func login(c *gin.Context) {
+	//post username and password to login 
+	//if User in token == "" generate token else return token
+
+	var user User
+	err := c.BindJSON(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	db := connectDB()
+	//post username and password to login 
+	//if User in token == "" generate token else return token
+
+	var username string
+	var password string
+	var token string
+	err = db.QueryRow("SELECT username, password, token FROM users WHERE username = $1", user.Username).Scan(&username, &password, &token)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "username or password is incorrect"})
+		return
+	}
+
+	if !checkPasswordHash(user.Password, password) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "username or password is incorrect"})
+		return
+	}
+
+	if token == "" {
+		token, err = generateToken(username, password)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		_, err = db.Exec("UPDATE users SET token = $1 WHERE username = $2", token, username)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": token})
+	
+}
+
+func logout(c *gin.Context) {
+	//post authotization bearer token user token remove from db
+
+	token := c.GetHeader("Authorization")
+	token = strings.TrimPrefix(token, "Bearer ")
+	claims := jwt.MapClaims{}
+	tkn, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+			return
+		}
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if !tkn.Valid {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	db := connectDB()
+	_, err = db.Exec("UPDATE users SET token = $1 WHERE username = $2", "", claims["username"])
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
+}
+	
